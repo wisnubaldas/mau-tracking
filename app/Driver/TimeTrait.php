94d4065f820070d0961c2fca96f4e85853c85480
wisnubaldas <wisnubaldas@gmail.com>
2022-10-31 23:45:31 +0700

    tambah durasi

--- app/Driver/TimeTrait.php
+++ app/Driver/TimeTrait.php
@@ -3,6 +3,27 @@ namespace App\Driver;
 use Carbon\Carbon;
 
 trait TimeTrait {
+    public $awal = 0;
+    public $akhir = 0;
+    public function durasi()
+    {
+        $seconds = ($this->akhir - $this->awal);
+        $hours = floor($seconds / 3600);
+        $mins = floor($seconds / 60 % 60);
+        $secs = floor($seconds % 60);
+        return $hours.":".$mins.":".$secs;
+
+    }
+    public function akhir()
+    {
+        $this->akhir = microtime(true);
+        return $this->akhir;
+    }
+    public function awal()
+    {
+        $this->awal = microtime(true); 
+        return $this->awal;
+    }
     public function tambah_jam(string $dt)
     {
         return Carbon::parse($dt);
--- app/Jobs/OutboundFactoryJob.php
+++ app/Jobs/OutboundFactoryJob.php
@@ -9,10 +9,14 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Repositories\WarehouseExportOutputPort;
+
+use App\Driver\LoggingRotateTrait;
+use App\Driver\TimeTrait;
+
 class OutboundFactoryJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+    use LoggingRotateTrait, TimeTrait;
     /**
      * Create a new job instance.
      *
@@ -31,6 +35,9 @@ class OutboundFactoryJob implements ShouldQueue
      */
     public function handle(WarehouseExportOutputPort $out)
     {
+        $this->awal();
         $out->save_outbound($this->data);
+        $this->akhir();
+        $this->debug_log(['durasi eksekusi save_outbound '. $this->durasi()],'export.log');
     }
 }
