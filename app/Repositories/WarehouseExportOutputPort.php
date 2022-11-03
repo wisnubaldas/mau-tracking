9dde703f1bb1dcb901055a70d4f64cdad8324bad
wisnubaldas <wisnubaldas@gmail.com>
2022-11-03 13:08:32 +0700

    job

--- app/Repositories/WarehouseExportOutputPort.php
+++ app/Repositories/WarehouseExportOutputPort.php
@@ -87,8 +87,9 @@ class WarehouseExportOutputPort extends WarehouseEntities{
     public function save_outbound($data)
     {
         foreach ($data as $v) {
-		dd($v);
-
+		
+            $this->debug_log(['message'=>$v]);
+            dd(); 
             $head = ['waybill_smu'=>$v['waybill_smu'],'hawb'=>$v['hawb']];
             ThOutbound::updateOrCreate($head,$v);
             $this->debug_log(['message'=>'Input th_outbound hawb '.$v['hawb']]);                
